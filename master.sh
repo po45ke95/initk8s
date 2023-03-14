e13066885aff0051727469f7c4ef4839d4b3d39d
Peter.Chang <po45ke95@gmail.com>
2023-03-14 12:19:52 +0800

    fix os version scripts.

--- master.sh
+++ master.sh
@@ -4,15 +4,11 @@ GRN='\033[1;32m' # notice
 YEL='\033[1;33m' # warning
 NC='\033[0m' # No Color
 
-printf "${GRN}==Prepare install kubernetes training environment for ubuntu 22.04==${NC}\n"
+printf "${GRN}==Prepare install kubernetes training environment for ubuntu 18.04==${NC}\n"
 MASTERNAME=$RANDOM
-sudo hostnamectl set-hostname master${MASTERNAME}
-
-#install net-tools for scripts
-sudo apt install net-tools -y
+sudo hostnamectl set-hostname master${MASTERNAME}.suserancher.lab
 IPNAME=$(ifconfig ens3 |grep inet|cut -d ' ' -f 10 |head -n 1)
-
-sudo echo "${IPNAME} master${MASTERNAME}" >> /etc/hosts
+sudo echo "${IPNAME} master${MASTERNAME}.suserancher.lab" >> /etc/hosts
 
 sleep 1
 
@@ -22,11 +18,11 @@ printf "${RED}==phase 1: modify file system==${NC}\n"
 OSVERSION=$(cat /etc/lsb-release |grep DISTRIB_CODENAME |cut -d '=' -f 2)
 
 # verify os info
-if [ "${OSVERSION}" = 'jammy' ];
+if [ "${OSVERSION}" = 'bionic' ];
 then
     printf "${YEL}--OS Verified-- ${NC}\n"
 else
-    printf "${RED}==OS is not Verified, use ubuntu 22.04==${NC}\n"
+    printf "${RED}==OS is not Verified, use ubuntu 18.0==${NC}\n"
     exit 0
 fi
 
--- worker.sh
+++ worker.sh
@@ -4,15 +4,11 @@ GRN='\033[1;32m' # notice
 YEL='\033[1;33m' # warning
 NC='\033[0m' # No Color
 
-printf "${GRN}==Prepare install kubernetes training environment for ubuntu 22.04==${NC}\n"
+printf "${GRN}==Prepare install kubernetes training environment for ubuntu 18.04==${NC}\n"
 WORKERNAME=$RANDOM
-sudo hostnamectl set-hostname worker${WORKERNAME}
-
-#install net-tools for scripts
-sudo apt install net-tools -y
+sudo hostnamectl set-hostname worker${WORKERNAME}.suserancher.lab
 IPNAME=$(ifconfig ens3 |grep inet|cut -d ' ' -f 10 |head -n 1)
-
-sudo echo "${IPNAME} worker${WORKERNAME}" >> /etc/hosts
+sudo echo "${IPNAME} worker${WORKERNAME}.suserancher.lab" >> /etc/hosts
 sleep 1
 
 printf "${RED}==phase 1: modify file system==${NC}\n"
@@ -21,11 +17,11 @@ printf "${RED}==phase 1: modify file system==${NC}\n"
 OSVERSION=$(cat /etc/lsb-release |grep DISTRIB_CODENAME |cut -d '=' -f 2)
 
 # verify os info
-if [ "${OSVERSION}" = 'jammy' ];
+if [ "${OSVERSION}" = 'bionic' ];
 then
     printf "${YEL}--OS Verified-- ${NC}\n"
 else
-    printf "${RED}==OS is not Verified, use ubuntu 22.04==${NC}\n"
+    printf "${RED}==OS is not Verified, use ubuntu 18.0==${NC}\n"
     exit 0
 fi
 
